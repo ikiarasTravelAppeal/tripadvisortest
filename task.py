4ed7595de7015f099f174d62026677a6923fdb55
Ioannis Kiaras <ioanni.kiaras@travelappeal.com>
2017-11-13 16:58:13 +0100

    Your Message8

--- task.py
+++ task.py
@@ -1,11 +1,10 @@
 from sklearn.base import BaseEstimator
-import os.path
+import argparse
+import pandas as pd
+import  os.path
 import time
 import tensorflow as tf
 import numpy as np
-import pandas as pd
-import argparse
-
 
 class TF_NN(BaseEstimator):
 
@@ -19,6 +18,24 @@ class TF_NN(BaseEstimator):
 		self.activation = activation
 		self.epochs = epochs
 		self.momentum = momentum
+		self.model_path = None
+		self.model_name = None
+
+	def set_model_path(self, path):
+		self.model_path = path
+
+	def set_model_name(self, name):
+		self.model_name = name
+
+	def get_model_path(self):
+		if self.model_path == None:
+			raise ValueError('You must set the path where saving model!')
+		return self.model_path
+
+	def get_model_name(self):
+		if self.model_name == None:
+			raise ValueError('You must set the name of the model!')
+		return self.model_name
 
 	def create_batch_generator(self, X, y, batch_size, shuffle):
 		X_copy = np.array(X)
@@ -46,10 +63,7 @@ class TF_NN(BaseEstimator):
 												  reuse=True)
 		return h
 
-	def neuron_layer_scratch(self, input, output, activation, name):
-		pass
-
-	def build(self, X, y, x_dim, n):
+	def build(self, x_dim):
 		##### placeholders for input
 		self.tf_x = tf.placeholder(dtype=tf.float32, shape=(None, x_dim), name='tf_x')
 		self.tf_y = tf.placeholder(dtype=tf.float32, shape=(None), name='tf_y')
@@ -66,8 +80,10 @@ class TF_NN(BaseEstimator):
 	def neural_net(self):
 		with tf.variable_scope('neural_layer'):
 			hidden = self.neuron_layer(self.tf_x, self.n_hidden[0], activation=self.activation, name='hidden1')
-			if len(self.n_hidden) == 2:
-				hidden = self.neuron_layer(hidden, self.n_hidden[1], activation=self.activation, name='hidden2')
+			if len(self.n_hidden) >= 2:
+				for i, lay in enumerate(self.n_hidden[1:]):
+					name = 'hidden' + repr(i+2)
+					hidden = self.neuron_layer(hidden, self.n_hidden[i+1], activation=self.activation, name=name)
 			self.final_layer = self.neuron_layer(hidden, 1, None, 'output')
 
 		with tf.variable_scope('loss'):
@@ -84,18 +100,21 @@ class TF_NN(BaseEstimator):
 			#optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate, name='AdamOptimizer')
 			self.optimizer = optimizer.minimize(self.loss)
 
-	def fit(self, X, y, shuffle=True):
+	def fit(self, X, y, shuffle=True, save=False):
+		self.build_graph(X.shape[1])
+		## run the model
+		self.sess = tf.Session(graph=self.g)
+		self.train(X, y, shuffle, save)
+
+	def build_graph(self, dim):
 		## build the model
 		with self.g.as_default():
 			tf.set_random_seed(self.random_seed)
-			self.build(X, y, X.shape[1], X.shape[0])
+			self.build(dim)
+			self.saver = tf.train.Saver()
 			self.init_op = tf.global_variables_initializer()
 
-		## run the model
-		self.sess = tf.Session(graph=self.g)
-		self.train(X, y, shuffle)
-
-	def train(self, X, y, shuffle):
+	def train(self, X, y, shuffle, save):
 		## initialize all variables
 		self.sess.run(self.init_op)
 		for epoch in range(self.epochs):
@@ -108,19 +127,34 @@ class TF_NN(BaseEstimator):
 				epoch_cost.append(batch_cost)
 
 			self.training_cost.append(np.mean(epoch_cost))
-			#if not (epoch+1) % 50: print('---- Epoch %2d: Avg. Training Loss: %.4f' % (epoch+1, np.mean(epoch_cost)))
 
-	def predict(self, X):
+			#### CHECKPOINT
+			if save:
+				if (epoch+1) %100 == 0:
+					self.saver.save(self.sess, self.get_model_path() + self.get_model_name() + '_%d.ckpt' %(epoch+1))
+			#if not (epoch+1) % 50: print('---- Epoch %2d: Avg. Training Loss: %.4f' % (epoch+1, np.mean(epoch_cost)))
+		if save:
+			self.saver.save(self.sess, self.get_model_path() + self.get_model_name() +'_final.ckpt')
+
+	def predict(self, X, fromSavedModel=False):
 		with tf.name_scope("predict"):
-			y_pred = self.sess.run(self.final_layer, feed_dict={self.tf_x: X})
+			if not fromSavedModel:
+				y_pred = self.sess.run(self.final_layer, feed_dict={self.tf_x: X})
+			else:
+				self.build_graph(X.shape[1])
+
+				with tf.Session(graph=self.g) as sess:
+					self.saver.restore(sess, self.get_model_path() + self.get_model_name() + '_final.ckpt')
+					#self.saver.restore(sess, tf.train.latest_checkpoint(self.get_model_path()))
+					y_pred = sess.run(self.final_layer, feed_dict={self.tf_x: X})
 		return y_pred
 
 	# def plotTrainingCost(self):
-		# plt.plot(range(1, len(self.training_cost) + 1), self.training_cost)
-		# plt.tight_layout()
-		# plt.xlabel('Epochs')
-		# plt.ylabel('Training Cost')
-		# plt.show()
+	# 	plt.plot(range(1, len(self.training_cost) + 1), self.training_cost)
+	# 	plt.tight_layout()
+	# 	plt.xlabel('Epochs')
+	# 	plt.ylabel('Training Cost')
+	# 	plt.show()
 
 
 if __name__ == "__main__":
@@ -158,10 +192,13 @@ if __name__ == "__main__":
 						9.3, 9, 10, 10.5, 11.9, 11.7, 12.5, 12, 12.2, 14])
 
 	model = TF_NN([15,10], learning_rate=0.0001, activation=tf.tanh, random_seed=0, batch=2, epochs=100)
+	model.set_model_name("testmodel")
+	model.set_model_path(args.job_dir)
+	# model.set_model_path("/Users/ikiaras/Desktop/tensorflowchart/modelsave/")
 
 	writer = tf.summary.FileWriter(args.job_dir, model.g)
 
-	model.fit(X, Y)
+	model.fit(X, Y,save=True)
 	#model.plotTrainingCost()
 	y_pred = model.predict(X)
 	print(y_pred)
